b8c4bec84dbf45cbfae91edde6bf8fa138a3b321
Wiermag <wierzbowska.magda@gmail.com>
2020-04-24 15:15:20 +0200

    Updates

---
+++ precache-manifest.dde353b9a120c01f5b95da6470b3e11e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98888e5377ff05c79b9b024dd28cdaf3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "34eeb749e3a31537670a",
+    "url": "./static/css/main.b862bda1.chunk.css"
+  },
+  {
+    "revision": "8172013fcf3d168976d9",
+    "url": "./static/js/2.00972dcf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.00972dcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34eeb749e3a31537670a",
+    "url": "./static/js/main.cc246346.chunk.js"
+  },
+  {
+    "revision": "cce685874b3d0063ab39",
+    "url": "./static/js/runtime-main.9712b533.js"
+  },
+  {
+    "revision": "da1ae86ac8d7bbac8760a85074c4b08e",
+    "url": "./static/media/a2b567130edaa552ed5c1a781e72d597_cute-doodle-kpop-kawaii-stars-aesthetic-parallel-clip-art-library_920-585.da1ae86a.jpeg"
+  },
+  {
+    "revision": "e80018caa10281aae9b346cede881d40",
+    "url": "./static/media/pinpng.com-stars-png-tumblr-2298324.e80018ca.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/API-NASA-React/public/precache-manifest.b1e136d45995cdc52f16afaa770dbf4f.js"
+  "./precache-manifest.dde353b9a120c01f5b95da6470b3e11e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/API-NASA-React/public/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
